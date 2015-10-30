4cf9394c9b8f6e9660df3bd70c71c81edf138cce
Prashant <prashantpacy@gmail.com>
2015-10-30 15:48:47 -0500

    adding PCA train test

---
+++ Code/PCATrainTest/src/main/scala/PCA.scala
@@ -0,0 +1,188 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.rdd._
+import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.linalg.distributed.RowMatrix
+// import org.apache.spark.mllib.linalg.distributed.DenseMatrix
+ // import org.apache.spark.mllib.rdd.VectorRDDS
+// import org.apache.spark.mllib.linalg.Vectors
+ import org.apache.spark.mllib.linalg.Matrices
+
+object PCA {
+  def main(args: Array[String]) {
+    // setting conf parameter to run the application
+    val conf = new SparkConf()
+        conf.setAppName("PCA")
+        conf.set("spark.storage.memoryFraction", "1");
+    val sc = new SparkContext(conf)
+
+    // reading train data
+
+    val data = sc.textFile("/data/kddcupdata/kddcup.trasfrom")
+    val labelData = data.map{line =>
+            val buffer = line.split(",").toBuffer
+            buffer.remove(1,3)
+            val label = buffer.remove(buffer.length-1)
+            val vector = Vectors.dense(buffer.map(_.toDouble).toArray)
+            (label,vector)
+        }
+
+   // run PCA on train data to get top k PCA 
+   val pcaK =10 
+   
+   val mat = new RowMatrix(labelData.values)
+
+   // Compute principal components.
+   val pc = mat.computePrincipalComponents(pcaK)
+
+   val projected = mat.multiply(pc).rows
+   // projected.saveAsTextFile("PCA10Data")
+
+   
+
+   // train the model with the new data m*k 
+
+   val numClusters = 5
+   val numIterations = 10     
+   val model = KMeans.train(projected, numClusters, numIterations)
+   
+   // run the train data back to model to get the clusterid Information
+    var trainind =0
+    val trainlabels = labelData.keys.toArray
+   //  println(labelTestData.keys)
+    val clusterstrainLabelCount =  projected.map {line =>
+        val cluster = model.predict(line)
+       // val label = trainlabels.apply(trainind)
+        var label = "normal"
+        if(trainlabels.apply(trainind) != "normal.") {
+           label = "attack"
+        }
+        trainind += 1
+
+    (cluster, label)
+        }.countByValue
+  
+   // change the others label as attack (if its not normal) 
+   
+   /* val clusterNormalAttack = clusterstrainLabelCount.toSeq.foreach{
+           case((cluster,label),count) =>
+             println(f"$cluster%1s$label%18s$count%8s")
+   
+     } */
+
+    var clustermap:Map[Int,String] = Map()
+    var i =0
+    for ( i <- 0 to numClusters-1) {
+        var normalCount =0L 
+        var attackCount =0L
+         clusterstrainLabelCount.toSeq.foreach{
+           case((cluster,label),count) =>
+             if(i == cluster && label == "normal") {
+                normalCount += count
+             } else if( i == cluster && label == "attack") {
+                attackCount += count
+             } 
+
+         }
+         if(normalCount > attackCount) {
+            clustermap += ( i -> "normal")    
+          } else if(attackCount > normalCount) {
+            clustermap += ( i -> "attack")
+         }
+    }   
+   
+    // print clustermap 
+    clustermap.foreach(println)
+    
+   // load test data and transform this to new m*k format 
+
+   val testData = sc.textFile("/data/kddcupdata/corrected")
+   
+   val labelTestData = testData.map{line => 
+            val buffer = line.split(",").toBuffer
+            buffer.remove(1,3)
+            val label = buffer.remove(buffer.length -1)
+            val vector = Vectors.dense(buffer.map(_.toDouble).toArray)
+            (label,vector)
+        }
+
+    // run for all test data at once 
+
+    
+    val testmat = new RowMatrix(labelTestData.values)
+    
+    val predictdata = testmat.multiply(pc).rows
+    var ind =0 
+    val testlabels = labelTestData.keys.toArray 
+   //  println(labelTestData.keys)  
+    
+    
+    val clustersLabelCount =  predictdata.map {line =>
+        val cluster = model.predict(line)
+        val label = testlabels.apply(ind)
+        ind += 1
+        
+    (cluster, label)
+        }.countByValue
+
+ 
+    // val prediction = model.predict(predictdata)
+    
+    // prediction.foreach(println)
+     
+     /* val clustersLabelCount = labelTestData.map { case(label,datum) =>
+            // val rows=sc.parallelize(datum)
+            // val datums =  Vectors.dense(datum.toArray)
+           //  val vectorRdd = VectorRDDs.fromArrayRDD(datum.toArray)
+           //  val vectorRDD = sc.makeRDD(Vectors.dense(datum.toArray))
+            // val pcaData = new RowMatrix(vectorRDD)
+            val pcaData = Matrices.dense(1,36,datum.toArray)
+            val matData = pcaData.multiply(pc).rows
+            // println(matData)
+            val cluster = model.predict(matData)
+            (cluster, label)
+        }.countByValue 
+    */
+    // cluster and count
+       var TP =1L 
+       var FP =1L
+       var FN =1L
+       var TN =1L
+       clustersLabelCount.toSeq.foreach {
+            case((cluster,label),count) =>
+        // println("label is : " + label)
+        // println("cluster is : " + cluster)
+        // println("actual cluster is : " + clustermap.get(cluster).get)
+         clustermap.get(cluster) match {
+         case Some(i) => 
+         if(label == "normal." && clustermap.get(cluster).get == "normal") {
+                TP += count
+         } else if (label != "normal." && clustermap.get(cluster).get =="normal" ) {
+                FP += count
+         } else if (label != "normal." && clustermap.get(cluster).get !="normal") {
+                TN +=  count
+         } else if (label == "normal." && clustermap.get(cluster).get != "normal") {
+                FN +=  count
+         }
+        case None => println("Cluster number is: " + cluster)  
+        }
+        
+    
+            
+            // println(f"$cluster%1s$label%18s$count%8s")
+        }  
+
+   println("TP is : " + TP)
+   println("FP is : " + FP)
+   println("TN is : " + TN)
+   println("FN is : " + FN)
+   println("precision is : " + TP/(TP+FP)) 
+   println("recall is : "+ TP/(TP+FN)) 
+   sc.stop()
+        
+ 
+  }
+}
