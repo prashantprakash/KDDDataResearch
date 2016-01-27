28e69ba00b93ed4266ed88cf5a26f199b1b77935
Prashant <prashantpacy@gmail.com>
2016-01-27 08:30:59 -0600

    adding svm code only for distance

---
+++ Code/svm/src/main/scala/svm.scala
@@ -0,0 +1,29 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import scala.io.Source
+
+object SVM {
+  def main(args: Array[String]) {
+    val filename = "/data/kddcupdata/supportvectors1";
+    val testFilename ="/data/kddcupdata/testdata1";
+    val rho = 49953 
+    val gamma = 1
+    for (line <- Source.fromFile(testFilename).getLines()) {
+         var kernel = 0.0
+         for (line1 <- Source.fromFile(filename).getLines()) {
+            kernel +=  Math.exp(gamma * Math.pow(Math.abs(line.toDouble - line1.toDouble),2))
+         }
+        kernel -= rho
+        println(kernel)
+
+    if(kernel>0) {
+        println("true")
+    } else {
+        println("false")
+      } 
+    }
+
+   // println(kernel)
+ }
+}
