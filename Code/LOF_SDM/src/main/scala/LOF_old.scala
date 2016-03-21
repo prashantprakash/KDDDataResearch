0f3afefb73a254dafc8ce4a7c1b796cc6d284a5c
Prashant <prashantpacy@gmail.com>
2016-03-21 10:32:46 -0500

    adding weighted mechanisim for LOF

--- Code/LOF_SDM/src/main/scala/LOF_old.scala
+++ Code/LOF_SDM/src/main/scala/LOF_old.scala
@@ -26,7 +26,7 @@ object LOF {
     val data = sc.textFile("/data/kddcupdata/kddcup.trasfrom.normal")
 
     // test data everything 
-    val testData =  sc.textFile("/data/kddcupdata/correctednoicmp")
+    val testData =  sc.textFile("/data/kddcupdata/corrected")
 
 
     // for feature expansion 
@@ -85,50 +85,11 @@ object LOF {
     // save the cluster centers in a map to use it later
     var clusterID =0
     var centroidMap:Map[Int,Vector] = Map() 
-    // build a map to store the count of points in all the cluster
-    var pointsCountMap:Map[Int, Long] = Map()
     model.clusterCenters.map{ line =>
         // print("cluster" + line)
         centroidMap += ( clusterID -> line )
-       //  pointsCountMap += ( clusterID -> 0 )
         clusterID += 1
     }
-
-    
-
-    // build a map to store the count of points in all the cluster 
-    // var pointsCountMap:Map[Int, Int] = Map()
-    val pointsPerCluster = projectedTrainData.map{line =>
-           
-           var clusterID = model.predict(line.features)
-           /* // println ("cluster id is : " + clusterID)
-           if (pointsCountMap contains clusterID ) {
-               // println ("already and cluster id is: " + clusertID  )
-                var count = pointsCountMap(clusterID)
-               // println ("count is : " + count)
-                 // println ("already and cluster id is: " + clusterID + " and previous count is : " + count )
-                pointsCountMap += (clusterID -> (count + 1))
-           } else{
-               //  println("first time")
-
-                println ("first and cluster id is: " + clusterID )
-
-                pointsCountMap += (clusterID -> 1)
-           }
-
-        println(pointsCountMap)*/
-
-        (clusterID)
-    }.countByValue
-    
-    pointsPerCluster.toSeq.sorted.foreach {
-            case((clusterID),count) =>
-             // println(f"$cluster%1s$label%18s$count%8s")
-            //  println(clusterID +" : Count :" + count)
-            pointsCountMap += (clusterID -> count)
-        }
- 
-  
   //  centroidMap.foreach(println)
     println("centroid map is done")
     val min_pts = 5
@@ -136,7 +97,7 @@ object LOF {
     // local_outlier_factor
     // get local outlier degree of all train data 
     val trainOutlierDegree = projectedTrainData.map{line =>
-        var outlierFactor = local_outlier_factor(min_pts,line.features,centroidMap,pointsCountMap)
+        var outlierFactor = local_outlier_factor(min_pts,line.features,centroidMap)
         (outlierFactor,1)
      }
 
@@ -144,13 +105,13 @@ object LOF {
 
 
     // take first 2% to decide threshold 
-     var firstN =  ((data.count() * 98)/100).toInt 
+     /* var firstN =  ((data.count() * 2)/100).toInt 
      val sample = trainOutlierDegree.sortByKey().take(firstN)
      // sample.foreach(println)
      val threshold = sample.last.toString.split(',')(0).toDouble
-     println(threshold)
+     println(threshold) */
 
-   // val threshold = 1.0 // as given in the code
+    val threshold = 1.0 // as given in the code
      
     // read test data and build features 
     
@@ -189,8 +150,8 @@ object LOF {
     // check for test data 
 
     val testOutlierDegree = projectedTestData.map{line =>
-        var outlierDegree = local_outlier_factor(min_pts,line.features,centroidMap,pointsCountMap)
-       // println(line.label+","+outlierDegree)
+        var outlierDegree = local_outlier_factor(min_pts,line.features,centroidMap)
+        println(line.label+","+outlierDegree)
         var classLabel = "noclass"
         if(line.label == 0.0 && outlierDegree < threshold) {
             // println("TP")
@@ -202,42 +163,35 @@ object LOF {
         } else if(line.label != 0.0 && outlierDegree > threshold) {
             classLabel = "TN"
         }
-        (classLabel)
-     }.countByValue
+        (classLabel,1)
+     }
     
-    // testOutlierDegree.foreach(println)   
+    testOutlierDegree.foreach(println)    
 
-     testOutlierDegree.toSeq.sorted.foreach {
-            case((classLabel),count) =>
-             // println(f"$cluster%1s$label%18s$count%8s")
-             println(classLabel +" : Count :" + count)
-        } 
-   
+
   
     }
 
 
     /*
         function to calculate k-distance of a point 
+
     */
 
     def k_distance(k: Int , instance : Vector, centoridMap : Map[Int,Vector])  = {
         // println("instance in k-distance is " + instance )
         var distances:Map[Double,Vector] = Map() 
-        var distanceIDMap : Map[Double,Int] = Map()
         for ((k,v) <- centoridMap) {
             var distance_value = Math.sqrt(Vectors.sqdist(instance, v))
             distances += ( distance_value -> v )
-            distanceIDMap += (distance_value -> k)
+
         }
         var distanceList = distances.toList.sortBy (_._1) 
         var neighbours = new Array[Vector](k)
-        var neighboursClusterID = new Array[Int](k)
         var k_distance = 0.0
         var index = 0
         for((k1,v) <- distances.take(k) ) {
             neighbours(index) = v
-            neighboursClusterID(index) = distanceIDMap(k1) // store the cluster id of k nearest neighbors
             if(index == k-1) {
                 k_distance = k1
             }
@@ -245,17 +199,18 @@ object LOF {
         }
         // print("k-distacce is : "+ k_distance)
         //[neighbours.extend(n[1]) for n in distances[:k]]
-        (k_distance, neighbours,neighboursClusterID)
+        (k_distance, neighbours)
     }
 
 
     /*
         function to calculate reachability distance of a point , which is maximum of k-distance and 
+
     */
 
     def reachability_distance(k : Int, instance1 : Vector , instance2 : Vector, centoridmap : Map[Int,Vector])  = {
          // get the k distance of the instance with its neighbors
-         val (k_distance_value, neighbours,neighboursClusterID) = k_distance(k, instance2, centoridmap)
+         val (k_distance_value, neighbours) = k_distance(k, instance2, centoridmap)
          // reachability distance is maximum distance between the k-th distance and euclidean distance 
         (Math.max(k_distance_value,Math.sqrt(Vectors.sqdist(instance1,instance2))))
 
@@ -264,12 +219,13 @@ object LOF {
 
     /*
         function to calculate LRD , local reachable density of a point 
+
     */
 
 
-    def local_reachability_density(min_pts : Int , instance : Vector, centroidMap : Map[Int,Vector],pointsCountMap : Map[Int,Long]) = {
+    def local_reachability_density(min_pts : Int , instance : Vector, centroidMap : Map[Int,Vector]) = {
         // get the k-distacne value and all k neighbors of given instance
-        val (k_distance_value, neighbours,neighboursClusterID) = k_distance(min_pts, instance, centroidMap)
+        val (k_distance_value, neighbours) = k_distance(min_pts, instance, centroidMap)
         var reachability_distances_array = Array.fill(neighbours.length){0.0} 
         for (i <- 0 until neighbours.length -1) {
             reachability_distances_array(i) = reachability_distance(min_pts, instance, neighbours(i),centroidMap)
@@ -283,20 +239,14 @@ object LOF {
     
     /*
         function to calculate local outlier factor for a given point 
+
     */
 
-    def local_outlier_factor(min_pts  : Int , instance: Vector, centoridmap : Map[Int,Vector] , pointsCountMap : Map[Int,Long]) = {
+    def local_outlier_factor(min_pts  : Int , instance: Vector, centoridmap : Map[Int,Vector]) = {
         // first calculate k-distance for the given point 
-        val (k_distance_value, neighbours,neighboursClusterID) = k_distance(min_pts, instance , centoridmap)
-        // weighting mechanism 
-        // first calculate total number of points in min_points clusters
-        var totalPoints = 0.0
-        for(i<-0  until neighboursClusterID.length -1) {
-            totalPoints += pointsCountMap(neighboursClusterID(i))
-        }
-
+        val (k_distance_value, neighbours) = k_distance(min_pts, instance , centoridmap)
         // calculate lrd value for the given point 
-        val instance_lrd = local_reachability_density(min_pts, instance, centoridmap,pointsCountMap)
+        val instance_lrd = local_reachability_density(min_pts, instance, centoridmap)
 
         val lrd_ratios_array = Array.fill(neighbours.length){0.0} 
 
@@ -305,8 +255,8 @@ object LOF {
         for( i <- 0 until neighbours.length - 1) {
             // we have to remove the processing row from map 
            // println(" processing neighbor " + neighbours(i))
-            val neighbour_lrd  = local_reachability_density(min_pts, neighbours(i), centoridmap,pointsCountMap)
-            lrd_ratios_array(i) = ( (pointsCountMap(neighboursClusterID(i)) / totalPoints )* neighbour_lrd)/instance_lrd
+            val neighbour_lrd  = local_reachability_density(min_pts, neighbours(i), centoridmap)
+            lrd_ratios_array(i) = (1.0 * neighbour_lrd)/instance_lrd
         }
 
         (lrd_ratios_array.sum / neighbours.length)
@@ -315,5 +265,3 @@ object LOF {
 
    
  }
-
-
